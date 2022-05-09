648fbd9642e1b0ed045a25e60e2d9d1fd600deb9
federico alegre <fedealegre@gmail.com>
2022-05-09 17:18:38 -0300

    Agregar archivos de proyecto.

---
+++ Bootcamp_4/Persona.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bootcamp_4
+{
+    class Persona
+    {
+        protected string nombre;
+        protected int edad;
+        protected int dni;
+
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+        public int Dni
+        {
+            get
+            {
+                return dni;
+            }
+            set
+            {
+                dni = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"\nNombre: {Nombre}\nEdad: {Edad}\nDNI: {Dni}";
+        }
+        
+        public bool EsMayorDeEdad()
+        {
+            if (Edad > 18) return true;
+            else return false;
+        }
+    }
+}
---
+++ Bootcamp_4/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Bootcamp_4
+{
+    class Program
+    {
+        Persona[] arrP = new Persona[3];
+        static void Ejercicio2(Program p)
+        {
+            //Program P = new Program();
+            LlenarArreglo(p.arrP, 0);
+            Console.WriteLine(p.arrP[0].ToString());
+            Console.WriteLine($"¿es mayor de edad? {p.arrP[0].EsMayorDeEdad()}");          
+            
+        }
+
+        static void Ejercicio1(Program p)
+        {
+            //Program arrP = new Program();
+            LlenarArreglo(p.arrP, 0);
+            MostrarArreglo(p.arrP, 0);
+        }
+
+        static void MostrarArreglo(Persona[] p, int i)
+        {
+            Console.WriteLine($"Persona {i+1}: {p[i].ToString()}");
+            i++;
+            if (i < 3) MostrarArreglo(p, i);
+
+        }
+
+        static void LlenarArreglo(Persona[] p, int i)
+        {
+            Persona aux = new Persona();
+            Console.Write($"Ingrese el nombre de la persona {i+1}: ");
+            aux.Nombre = Console.ReadLine();
+            aux.Dni = TryAndCatchInt($"Ingrese el DNI de la persona {i+1}");
+            aux.Edad = TryAndCatchInt($"Ingrese la Edad de la persona {i + 1}");
+            p[i] = aux;
+            i++;
+            if (i < 3) LlenarArreglo(p, i);
+            
+        }
+
+        static int TryAndCatchInt(string msg)
+        {
+            bool f = false;
+            int option = 0;
+            do
+            {
+                Console.WriteLine(msg);
+                string s = Console.ReadLine();
+                try
+                {
+                    option = int.Parse(s);
+                    f = true;
+
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Ingreso un dato invalido!!");
+                }
+            } while (f != true);
+            return option;          
+
+        }
+
+        static void Menu(int o, Program p)
+        {
+            if (o > 0)
+            {
+                switch (o)
+                {
+                    case 1:
+                        {
+                            Ejercicio1(p);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Ejercicio2(p);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Beep();
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("NUMERO INVALIDO");
+                            break;
+                        }
+                }
+                Console.ReadKey();
+                Console.Clear();
+                Menu(TryAndCatchInt("Menu:\n0: Apagar\n1: Ejercicio 1\n2: Ejercicio 2\n3: Honk! "), p);
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Program person = new Program();
+            Console.WriteLine("Challenge 4");
+            Menu(TryAndCatchInt("Menu:\n0: Apagar"), person);            
+        }
+
+    }
+}
